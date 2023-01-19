b76177274e7d781611581a4fe4a326a5e018b82d
PatrickMcGrath9 <116979743+PatrickMcGrath9@users.noreply.github.com>
2023-01-19 08:28:07 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,109 @@
+i1 = 2
+i2 = 5
+i3 = -3
+d1 = 2.0
+d2 = 5.0
+d3 = -0.5
+
+'''print(i1 + i2)
+
+print(i1 / i2)
+
+print(i1 // i2)
+
+print(i2 / i1)
+
+print(i2 // i1)
+
+print(i1 * i3)
+
+print(d1 + d2)
+
+print(d1 / d2)
+
+print(d2 / d1)
+
+print(d3 * d1)
+
+print(d1 + i2)
+
+print(i1 / d2)
+
+print(d2 / i1)
+
+print(i2 / d1)
+
+print(i1/i2*d1)
+
+print(d1*i1/i2)
+
+print(d1/d2*i1)
+
+print(i1*d1/d2)
+
+print(i2/i1*d1)
+
+print(d1*i2/i1)
+
+print(d2/d1*i1)
+
+print(i1*d2/d1)
+
+print(i1 + (i2*i3))
+
+print(i1*(i2+i3))
+
+print(i1/(i2+i3))
+
+print(i1//(i2+i3))
+
+print(i1/i2+i3)
+
+print(i1//i2+i3)
+
+print(3+4+5/3)
+
+print(3+4+5//3)
+
+print((3+4+5)/3)
+
+print((3+4+5)//3)
+
+print(d1+(d2*d3))
+
+print(d1 + d2*d3)
+
+print(d1/d2-d3)
+
+print(d1/(d2-d3))
+
+print(d1+d2+d3/3)
+
+print((d1+d2+d3)/3)
+
+print(d1+d2+(d3/3))
+
+print(3*(d1+d2)*(d1-d3))
+
+
+n1 = int (input()) ; n2 = int (input())
+
+print(n1 ,n2)
+'''
+
+# Get two numbers from the user
+n1 = int(input());
+n2 = int(input())
+# Compute sum of the two numbers  	 #1
+print(n1 + n2)
+# Compute average of the two numbers  	#2
+print((n1 + n2) / 2)
+# Assign some variables 	#3
+d1 = d2 = 0
+# Compute a quotient 	#4
+print(n1 / d1)
+# Compute a product 	#5
+n1 * n2 = d1
+# Print result 	#6
+print(d1)  # 7
+
